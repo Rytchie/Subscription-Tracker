bbc55cba5d2cc952b28f62197ee4d41964bff0ee
Rytchie <rytchie1957@gmail.com>
2025-02-26 10:13:49 +0300

    Added a README.md file for testing

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import {signUp, signIn, signOut} from "../controllers/auth.controller.js";
 
+// Rytchie is a Programmer!
+
 const authRouter = Router();
 
 // Path: /api/v1/signup (POST)
